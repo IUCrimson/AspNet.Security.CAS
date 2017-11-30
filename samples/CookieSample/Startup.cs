228c8449d70e21f79b627b5b8585b332a1a9a3d6
Dave DeMeulenaere <davidmdem@gmail.com>
2017-11-30 10:37:55 -0500

    Update sample to simplify JS dependencies
    - Just use CDN instead of Bower
    - Got rid of unused boilerplate site.js

--- samples/CookieSample/Startup.cs
+++ samples/CookieSample/Startup.cs
@@ -1,10 +1,8 @@
 ﻿using AspNetCore.Security.CAS;
 using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
